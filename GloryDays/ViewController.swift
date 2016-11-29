623ac8b09e02e1c31243d85aeaf4b66388c179f0
Juan Manuel Jimenez Sanchez <juanmjimenezs@MacBook-Pro-de-Juan.local>
2016-11-28 22:11:07 -0500

    Initial Commit

---
+++ GloryDays/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GloryDays
+//
+//  Created by Juan Manuel Jimenez Sanchez on 28/11/16.
+//  Copyright © 2016 Juan Manuel Jimenez Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
